408f3e6f042926f5f95a3f5486b7dfd81cf9ad79
Nabhan Yuzqi <30825747+nabhanyuzqi1@users.noreply.github.com>
2023-06-01 02:35:49 +0700

    Update

--- app/src/main/java/com/oneplatform/obeng/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/oneplatform/obeng/ui/screen/HomeScreen.kt
@@ -1,14 +1,23 @@
 package com.oneplatform.obeng.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(){
     Text(modifier = Modifier.fillMaxSize(),text = "Testing")
+
+    LazyColumn(Modifier.background(Color.Red)){
+        items(10){
+            Text(text = "Hello Rakha!")
+        }
+    }
 }
 
 
